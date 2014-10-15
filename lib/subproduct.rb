dff2d604cea16a2e671a6bfe073ce9ff2f72036f
Raul Ferrandez <raul.ferrandez@here.com>
2014-10-15 17:02:15 +0200

    Ongoing ..

--- lib/subproduct.rb
+++ lib/subproduct.rb
@@ -1,23 +1,16 @@
 class SubProduct
-  attr_reader :main_product
+  attr_reader :name
   attr_reader :weight
+  attr_reader :quantity
 
   def initialize(params)
-    @main_product = params[:main_product]
+    @name = params[:name]
     @weight = params[:weight]
+    @quantity = params[:quantity]
   end
 
   def to_s
-    # Align to the left with a size of 10 chars
-    name_formatted = '%-20.20s' % @main_product.name
-    p_tienda = "%4.2f" % @main_product.price_tienda.to_f
-    p_coope = "%4.2f" % @main_product.price_coope.to_f
-    p_pvp = "%4.2f" % @main_product.price_pvp.to_f
-
-    price_formatted = "(#{p_tienda.to_s},#{p_coope.to_s},#{p_pvp.to_s})  EUR/KG"
-
-    #name_formatted + price_formatted + subproducts_formatted
-    name_formatted
+    "#{@quantity} x #{@weight} #{@name}"
   end
 
 end
