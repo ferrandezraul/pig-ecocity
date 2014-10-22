f7b7da7d55d6cd917c95274ec50a894de80ae54c
Raul Ferrandez <ferrandezraul@gmail.com>
2014-10-22 17:29:03 +0200

    Dev

--- new_order_dialog.rb
+++ new_order_dialog.rb
@@ -7,21 +7,23 @@ class NewOrderDialog
     # Call @stack.app in order to be able to access global @orders and global @gui_main_window
     @stack.app do
       @gui_main_window.clear{
-        border black
-        para "Data:", :margin => 4
-        date = edit_line "#{Date.today.to_s}", :margin => 4
+        stack :margin => 4 do
+          border black
+          para "Data:", :margin => 4
+          date = edit_line "#{Date.today.to_s}", :margin => 4
 
-        para "Client:", :margin => 4
-        customer_name = list_box items: @customer_names, :margin => 4
+          para "Client:", :margin => 4
+          customer_name = list_box items: @customer_names, :margin => 4
 
-        button "Acceptar", :margin => 4 do
-          begin
-            customer = CustomerHelper.find_customer_with_name( @customers, customer_name.text)
-          rescue Errors::CustomerHelperError
-            alert "Selecciona un client"
+          button "Acceptar", :margin => 4 do
+            begin
+              customer = CustomerHelper.find_customer_with_name( @customers, customer_name.text)
+            rescue Errors::CustomerHelperError
+              alert "Selecciona un client"
+            end
+
+            # TODO create new dialog for items
           end
-
-          # TODO create new dialog for items
         end
       }
     end
