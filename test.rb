960cb9d9e17a7a84c099b1d8b4beafefb567bf79
Raul Ferrandez <raul.ferrandez@here.com>
2014-10-23 11:54:23 +0200

    Nice example on test

--- test.rb
+++ test.rb
@@ -1,33 +1,80 @@
+$:.unshift File.join( File.dirname( __FILE__ ), "lib" )
+
+require 'product_csv'
+
 class MenuPanel < Shoes::Widget
 
   def initialize( args )
-    para link("Productes").click { visit "/view_products" }, :margin => 4
-    para link("Clients").click { visit "/view_customers" }, :margin => 4
-    para link("Comandes").click { visit "/view_orders" }, :margin => 4
-    para link("Nova Comanda").click { visit "/new_order" }, :margin => 4
+    @products = args[:products]
+    print_links
 
     # Use yield here to implement something from the caller??
   end
 
   def view_products
-    debug("Sii")
-    para "Estos son mis productos"
+    clear do
+      print_links
+      products_view @products
+    end
   end
 
   def view_customers
-    para "Estos son mis clientes"
+    clear do
+      print_links
+      para "Estos son mis clientes"
+    end
   end
 
   def view_orders
-    para "Estas son mis comandas"
+    clear do
+      print_links
+      para "Estas son mis comandas"
+    end
   end
 
   def new_order
-    para "Lets create an order!!"
+    clear do
+      print_links
+      para "Lets create an order!!"
+    end
+  end
+
+  def print_links
+    para link("Productes").click { view_products }, :margin => 4
+    para link("Clients").click { view_customers }, :margin => 4
+    para link("Comandes").click { view_orders }, :margin => 4
+    para link("Nova Comanda").click { new_order }, :margin => 4
   end
 
 end
 
+
+class ProductsView < Shoes::Widget
+  def initialize( products )
+    stack do
+      products.each do |product|
+        para "#{product.to_s}"
+      end
+    end
+  end
+end
+
+def products_csv__path
+  return ::File.join( File.dirname( __FILE__ ), "csv/products.csv" )
+end
+
+def load_products
+  debug( "Loading Products ..." )
+  begin
+    ProductCSV.read( products_csv__path )
+  rescue Errors::ProductCSVError => e
+    alert e.message
+  end
+end
+
+
 Shoes.app :width => 600, :height => 130 do
-  menu_panel :width => 500, :height => 500, :margin => 4
+  @products = load_products
+
+  menu_panel :products => @products
 end
