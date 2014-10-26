148d2999e13ad3627c7380d5aa7e260fdf3e3948
Raul Ferrandez <ferrandezraul@gmail.com>
2014-10-26 13:50:50 +0100

    Start refactoring orders views into a table

--- orders_view.rb
+++ orders_view.rb
@@ -2,9 +2,40 @@ $:.unshift File.join( File.dirname( __FILE__ ), "lib" )
 
 class OrdersView < Shoes::Widget
   def initialize( orders )
-    stack do
+    stack :margin => 4 do
+      headers
       orders.each do |order|
-        para "#{order.to_s}"
+        flow :margin => 4 do
+          stack :width => '25%' do
+            para "#{order.date}", :margin => 4, :align => 'left'
+          end
+          stack :width => '25%' do
+            para "#{order.customer.name}", :margin => 4, :align => 'right'
+          end
+          stack :width => '25%' do
+            para "#{ order.order_items}", :margin => 4, :align => 'right'
+          end
+          stack :width => '25%' do
+            para "#{ '%.2f' % order.total} €", :margin => 4, :align => 'right'
+          end
+        end
+      end
+    end
+  end
+
+  def headers
+    flow :margin => 4 do
+      stack :width => '25%' do
+        para strong("DATE"), :margin => 4, :align => 'left'
+      end
+      stack :width => '25%' do
+        para strong("CUSTOMER"), :margin => 4, :align => 'right'
+      end
+      stack :width => '25%' do
+        para strong("PRODUCTES"), :margin => 4, :align => 'right'
+      end
+      stack :width => '25%' do
+        para strong("TOTAL"), :margin => 4, :align => 'right'
       end
     end
   end
